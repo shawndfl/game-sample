65d3ddd85efe0be9164e3c7510d2bf8de4d59f5e
Shawn <>
2019-04-10 04:35:22 -0700

    Can now accept clients

--- src/BskHttpServer.cc
+++ src/BskHttpServer.cc
@@ -11,13 +11,29 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <pthread.h>
+#include <signal.h>
+#include <errno.h>
 
 #include "BskLogging.h"
 
 #include "BskHttpServer.h"
 
+static pthread_mutex_t messageLock = PTHREAD_MUTEX_INITIALIZER;
+#define BACKLOG 50
+
+void* acceptEvent(void * context);
+void* readEvent(void * context);
+
+typedef struct ListenContext {
+   int port;
+   BskHttpServer* server;
+}ListenContext;
+
 /*************************************************/
-BskHttpServer::BskHttpServer() {
+BskHttpServer::BskHttpServer() :
+_acceptThread(0),
+_serverSocket(-1) {
 
 }
 
@@ -27,11 +43,84 @@ BskHttpServer::~BskHttpServer() {
 }
 
 /*************************************************/
-void BskHttpServer::StartServer(int port) {
-   int server_fd, new_socket, valread;
-   struct sockaddr_in address;
+void* acceptEvent(void * context) {
+
+   ListenContext* listenContext = (ListenContext*) context;
+   volatile int serverSocket;
+
+   // Creating socket file descriptor
+   if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+      LOGE("socket error");
+      return nullptr;
+   }
+
+   // Forcefully attaching socket to the port 8080
    int opt = 1;
-   int addrlen = sizeof(address);
+   if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
+         sizeof(opt)) == -1) {
+      int e = errno;
+      LOGE("setsockopt error %d", e);
+      return nullptr;
+   }
+
+   struct sockaddr_in address;
+   //memset(&address, 0, sizeof(sockaddr_in));
+   address.sin_family = AF_INET;
+   address.sin_addr.s_addr = INADDR_ANY;
+   address.sin_port = htons(listenContext->port);
+
+   // Forcefully attaching socket to the port 8080
+   if (bind(serverSocket, (struct sockaddr *) &address, sizeof(address)) < 0) {
+      LOGE("Error binding to port %d", listenContext->port);
+      return nullptr;
+   }
+   if (listen(serverSocket, BACKLOG) < 0) {
+      LOGE("Error listen %d %d", serverSocket, errno);
+      return nullptr;
+   }
+
+   LOGI("Listening on port %d", listenContext->port);
+
+   while (true) {
+
+      struct sockaddr clientAddr;
+      socklen_t addrLen = sizeof(clientAddr);
+      int clientfd = accept(serverSocket, &clientAddr, &addrLen);
+      if (clientfd < 0) {
+         LOGE("Error accept %d %d", clientfd, errno);
+         exit(EXIT_FAILURE);
+      }
+
+      LOGI("Got a client %d", clientfd);
+   }
+
+   delete listenContext;
+   return nullptr;
+}
+
+/*************************************************/
+void* readEvent(void * context) {
+   int socketfd = *(int*) context;
+
+   while (true) {
+      //valread = read(new_socket, buffer, 1024);
+      //LOGI("Read %d bytes", valread);
+      //LOGI("%s", buffer);
+
+      //int len = strlen(hello);
+      // LOGI("Sending %s %d", hello, len);
+      // send(new_socket, hello, len, 0);
+      //sleep(1);
+
+      LOGI("listening on socket %d", socketfd);
+      //close (server_fd);
+      // close (new_socket);
+   }
+   return nullptr;
+}
+
+/*************************************************/
+void BskHttpServer::StartServer(int port) {
    char buffer[1024] = { 0 };
    const char *hello = "HTTP/1.1 200 OK\r\n"
          "Content-Type: text/html; charset=ISO-8859-1\r\n"
@@ -39,49 +128,24 @@ void BskHttpServer::StartServer(int port) {
          "Content-Length: 15\r\n\r\n"
          "Hello 99";
 
-   // Creating socket file descriptor
-   if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-      LOGE("socket error");
-      return;
+   if (signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
+      LOGE("Cannot filter SIGPIPE");
    }
 
-   // Forcefully attaching socket to the port 8080
-   if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
-         sizeof(opt))) {
-      LOGE("setsockopt error");
-      return;
-   }
-   address.sin_family = AF_INET;
-   address.sin_addr.s_addr = INADDR_ANY;
-   address.sin_port = htons(port);
+   ListenContext* context = new ListenContext();
+   context->server = this;
+   context->port = port;
 
-   // Forcefully attaching socket to the port 8080
-   if (bind(server_fd, (struct sockaddr *) &address, sizeof(address)) < 0) {
-      LOGE("Error binding to port %d", port);
-      return;
-   }
-   if (listen(server_fd, 3) < 0) {
-      LOGE("Error listening %d", port);
-      exit(EXIT_FAILURE);
-   }
-   if ((new_socket = accept(server_fd, (struct sockaddr *) &address,
-         (socklen_t*) &addrlen)) < 0) {
-      LOGE("Error accept");
-      exit(EXIT_FAILURE);
-   }
+   pthread_attr_t attr;
+   pthread_attr_init(&attr);
+   pthread_create(&_acceptThread, &attr, &acceptEvent, context);
+   pthread_attr_destroy(&attr);
 
-   //while (true) {
-      valread = read(new_socket, buffer, 1024);
-      LOGI("Read %d bytes", valread);
-      LOGI("%s", buffer);
+   int ret;
+   int* retp = &ret;
 
-      int len = strlen(hello);
-      LOGI("Sending %s %d", hello, len);
-      send(new_socket, hello, len, 0);
-      sleep(1);
+   pthread_join(_acceptThread, (void**) &retp);
 
-      LOGI("Sent Message");
-      close(server_fd);
-      close(new_socket);
-   //}
+   LOGI("Join main thread %d", ret);
 }
+
--- src/BskHttpServer.h
+++ src/BskHttpServer.h
@@ -8,13 +8,25 @@
 #ifndef SRC_BSKHTTPSERVER_H_
 #define SRC_BSKHTTPSERVER_H_
 
+#include <queue>
+#include <pthread.h>
+
 class BskHttpServer {
 public:
+   struct Message {
+      void* message;
+      long size;
+   };
+
    BskHttpServer();
    virtual ~BskHttpServer();
 
    void StartServer(int port);
 
+private:
+   std::queue<BskHttpServer::Message> _messages;
+   pthread_t _acceptThread;
+   int _serverSocket;
 };
 
 #endif /* SRC_BSKHTTPSERVER_H_ */
