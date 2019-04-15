a914020346a5822872c252c37264900fd6503385
Shawn <>
2019-04-15 14:45:07 -0700

    Run Nginx

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -8,4 +8,10 @@ if [ ! -f /etc/nginx/sites-enabled/${site_name} ]; then
 	sudo ln -s /etc/nginx/sites-available/${site_name} /etc/nginx/sites-enabled/${site_name}
 fi
 
-sudo sed -i 's:{{WWW_ROOT}}:'${home_base}'/www:' /etc/nginx/sites-enabled/${site_name}
+sudo sed -i 's:{{WWW_ROOT}}:'${home_base}'/www:' /etc/nginx/sites-available/${site_name}
+
+sudo rm -f /etc/nginx/sites-enabled/default
+sudo systemctl restart nginx.service
+sudo systemctl restart php7.2-fpm
+
+sudo nginx -t
