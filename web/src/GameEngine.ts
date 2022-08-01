ec00ea5ad680fa182e3718fe9a6fbcf31b29f378
shawn <shawn@localhost.com>
2022-07-31 22:30:59 -0400

    Add audoio

--- web/src/GameEngine.ts
+++ web/src/GameEngine.ts
@@ -2,42 +2,63 @@ import CameraController from './controllers/CameraController';
 import Terrain from './Terrain';
 
 import {
-    Scene, Renderer, PerspectiveCamera, WebGLRenderer, Mesh, MeshBasicMaterial, BoxGeometry, Clock, Vector3
-}
-from "three";
+    Scene,
+    Renderer,
+    PerspectiveCamera,
+    WebGLRenderer,   
+    Clock,
+    Vector3,
+    AudioListener,
+    Audio,
+    AudioLoader
+} from "three";
 import CharacterController from './controllers/CharacterController';
 import CharacterComponent from './components/CharacterComponent';
 
+import bgSound from './assets/sound/TownTheme.mp3'
+
 export default class GameEngine {
-    private _scene: Scene;
-    private _camera: PerspectiveCamera;
-    private _cameraController: CameraController;
+    private _scene : Scene;
+    private _camera : PerspectiveCamera;
+    private _cameraController : CameraController;
 
-    private _characterCtl: CharacterController;
-    private _characterComp: CharacterComponent;
+    private _characterCtl : CharacterController;
+    private _characterComp : CharacterComponent;
 
-    private renderer: Renderer;
-    private _clock: Clock;
+    private renderer : Renderer;
+    private _clock : Clock;
 
     private _terrain : Terrain;
+    private _audio : Audio;
 
-    constructor(private _container: HTMLElement) {
+    constructor(private _container : HTMLElement) {
         this._scene = new Scene();
         this._clock = new Clock();
+        const listen = new AudioListener ();
 
         this._camera = new PerspectiveCamera(45, 1.25, .01, 1000);
-        this.renderer = new WebGLRenderer();        
+        this._camera.add(listen);
+        this.renderer = new WebGLRenderer();
         this._cameraController = new CameraController(this._container, this._camera);
-        this._camera.position.add(new Vector3(0,1,0));
+        this._camera.position.add(new Vector3(0, 1, 0));
 
         this._terrain = new Terrain();
         this._initialize(this._container)
 
         // make a character
-        this._characterComp = new CharacterComponent({});        
+        this._characterComp = new CharacterComponent({});
         this._characterCtl = new CharacterController(this._characterComp);
 
         this._scene.add(this._characterComp);
+        this._audio = new Audio(listen);
+
+        const audioLoader = new AudioLoader();
+        audioLoader.load(bgSound, (buffer) => {
+            this._audio.setBuffer(buffer);
+            this._audio.setLoop(true);
+            this._audio.setVolume(0.5);
+            this._audio.play();
+        });
     }
 
 
@@ -48,11 +69,12 @@ export default class GameEngine {
     private _initialize(container : HTMLElement) {
 
 
-
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         container.appendChild(this.renderer.domElement);
 
-        window.addEventListener("resize", (e:UIEvent)=> { this.onResize(e)})        
+        window.addEventListener("resize", (e : UIEvent) => {
+            this.onResize(e)
+        })
 
         this._scene.add(this._terrain.mesh);
 
@@ -61,23 +83,22 @@ export default class GameEngine {
         requestAnimationFrame(this.update);
     }
 
-    onResize(e:UIEvent) {
+    onResize(e : UIEvent) {
         this._camera.aspect = window.innerWidth / window.innerHeight;
         this._camera.updateProjectionMatrix();
         this.renderer.setSize(this._container.clientWidth, this._container.clientHeight);
         this._cameraController.onResize();
     }
 
-    update = (() => { 
-        // get dt
+    update = (() => { // get dt
         const dt = this._clock.getDelta();
-        
+
         // update stuff
         this._cameraController.update(dt);
-        requestAnimationFrame(this.update); 
+        requestAnimationFrame(this.update);
 
-        //render
-        this.renderer.render(this._scene, this._camera); 
+        // render
+        this.renderer.render(this._scene, this._camera);
     });
 
 
--- web/webpack.dev.js
+++ web/webpack.dev.js
@@ -28,6 +28,10 @@ module.exports = {
             {
                 test: /\.png$/,
                 use: ["url-loader"]
+            },
+            {
+                test: /\.mp3$/,
+                use: ["url-loader"]
             }
         ]
     },
